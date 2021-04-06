461153a94f2f7ef2066aaff5b5c7794b9676a671
Pyython <vogonpoetry06@gmail.com>
2021-04-06 16:15:19 +0700

    tipjar docs + event

--- contracts/TipJar.sol
+++ contracts/TipJar.sol
@@ -47,6 +47,9 @@ contract TipJar is AccessControlUpgradeable, CheckAndSend {
     /// @notice Miner split updated event
     event MinerSplitUpdated(address indexed miner, address indexed newSplitTo, address indexed oldSplitTo, uint32 newSplit, uint32 oldSplit);
 
+    /// @notice Tip event
+    event Tip(address indexed miner, address indexed tipper, uint256 tipAmount, uint256 splitAmount, uint256 feeAmount, address feeCollector);
+
     /// @notice modifier to restrict functions to admins
     modifier onlyAdmin() {
         require(hasRole(TIP_JAR_ADMIN_ROLE, msg.sender), "Caller must have TIP_JAR_ADMIN_ROLE role");
@@ -96,6 +99,12 @@ contract TipJar is AccessControlUpgradeable, CheckAndSend {
     /// @notice Fallback function to allow contract to accept ETH
     fallback() external payable {}
 
+    /**
+     * @notice Check that contract call results in specific 32 bytes value, then transfer ETH
+     * @param _target target contract
+     * @param _payload contract call bytes
+     * @param _resultMatch result to match
+     */
     function check32BytesAndSend(
         address _target,
         bytes calldata _payload,
@@ -104,6 +113,12 @@ contract TipJar is AccessControlUpgradeable, CheckAndSend {
         _check32Bytes(_target, _payload, _resultMatch);
     }
 
+    /**
+     * @notice Check that contract call results in specific 32 bytes value, then tip
+     * @param _target target contract
+     * @param _payload contract call bytes
+     * @param _resultMatch result to match
+     */
     function check32BytesAndTip(
         address _target,
         bytes calldata _payload,
@@ -113,6 +128,12 @@ contract TipJar is AccessControlUpgradeable, CheckAndSend {
         tip();
     }
 
+    /**
+     * @notice Check that multiple contract calls result in specific 32 bytes value, then transfer ETH
+     * @param _targets target contracts
+     * @param _payloads contract call bytes
+     * @param _resultMatches results to match
+     */
     function check32BytesAndSendMulti(
         address[] calldata _targets,
         bytes[] calldata _payloads,
@@ -121,6 +142,12 @@ contract TipJar is AccessControlUpgradeable, CheckAndSend {
         _check32BytesMulti(_targets, _payloads, _resultMatches);
     }
 
+    /**
+     * @notice Check that multiple contract calls result in specific 32 bytes value, then tip
+     * @param _targets target contracts
+     * @param _payloads contract call bytes
+     * @param _resultMatches results to match
+     */
     function check32BytesAndTipMulti(
         address[] calldata _targets,
         bytes[] calldata _payloads,
@@ -130,6 +157,12 @@ contract TipJar is AccessControlUpgradeable, CheckAndSend {
         tip();
     }
 
+    /**
+     * @notice Check that contract call results in specific bytes value, then transfer ETH
+     * @param _target target contract
+     * @param _payload contract call bytes
+     * @param _resultMatch result to match
+     */
     function checkBytesAndSend(
         address _target,
         bytes calldata _payload,
@@ -138,6 +171,12 @@ contract TipJar is AccessControlUpgradeable, CheckAndSend {
         _checkBytes(_target, _payload, _resultMatch);
     }
 
+    /**
+     * @notice Check that contract call results in specific bytes value, then tip
+     * @param _target target contract
+     * @param _payload contract call bytes
+     * @param _resultMatch result to match
+     */
     function checkBytesAndTip(
         address _target,
         bytes calldata _payload,
@@ -147,6 +186,12 @@ contract TipJar is AccessControlUpgradeable, CheckAndSend {
         tip();
     }
 
+    /**
+     * @notice Check that multiple contract calls result in specific bytes value, then transfer ETH
+     * @param _targets target contracts
+     * @param _payloads contract call bytes
+     * @param _resultMatches results to match
+     */
     function checkBytesAndSendMulti(
         address[] calldata _targets,
         bytes[] calldata _payloads,
@@ -155,6 +200,12 @@ contract TipJar is AccessControlUpgradeable, CheckAndSend {
         _checkBytesMulti(_targets, _payloads, _resultMatches);
     }
 
+    /**
+     * @notice Check that multiple contract calls result in specific bytes value, then tip
+     * @param _targets target contracts
+     * @param _payloads contract call bytes
+     * @param _resultMatches results to match
+     */
     function checkBytesAndTipMulti(
         address[] calldata _targets,
         bytes[] calldata _payloads,
@@ -168,22 +219,28 @@ contract TipJar is AccessControlUpgradeable, CheckAndSend {
      * @notice Distributes any ETH in contract to relevant parties
      */
     function tip() public payable {
+        uint256 tipAmount;
+        uint256 feeAmount;
+        uint256 splitAmount;
         if (networkFee > 0) {
-            uint256 fee = (address(this).balance * networkFee) / 1000000;
-            (bool feeSuccess, ) = networkFeeCollector.call{value: fee}("");
+            feeAmount = (address(this).balance * networkFee) / 1000000;
+            (bool feeSuccess, ) = networkFeeCollector.call{value: feeAmount}("");
             require(feeSuccess, "Could not collect fee");
         }
 
         if(minerSplits[block.coinbase].splitPct > 0) {
-            uint256 split = (address(this).balance * minerSplits[block.coinbase].splitPct) / 1000000;
-            (bool splitSuccess, ) = minerSplits[block.coinbase].splitTo.call{value: split}("");
+            splitAmount = (address(this).balance * minerSplits[block.coinbase].splitPct) / 1000000;
+            (bool splitSuccess, ) = minerSplits[block.coinbase].splitTo.call{value: splitAmount}("");
             require(splitSuccess, "Could not split");
         }
 
         if (address(this).balance > 0) {
-            (bool success, ) = block.coinbase.call{value: address(this).balance}("");
+            tipAmount = address(this).balance;
+            (bool success, ) = block.coinbase.call{value: tipAmount}("");
             require(success, "Could not collect ETH");
         }
+        
+        emit Tip(block.coinbase, msg.sender, tipAmount, splitAmount, feeAmount, networkFeeCollector);
     }
 
     /**
